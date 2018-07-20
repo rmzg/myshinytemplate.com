eb14c1e9a28bfa473ce33890aa0e3eb05e8aff54
Will Braswell <william.braswell@autoparallel.com>
2018-07-20 01:17:10 -0500

    Backup, Add Option To Include .user Table, Part 2

--- modified/git_backup__myshinytemplate.com__no_user.sh
+++ modified/git_backup__myshinytemplate.com__no_user.sh
@@ -2,20 +2,30 @@
 DATE=`date +%Y%m%d`
 DIR=/home/NEED_USERNAME/public_html/myshinytemplate.com-latest
 
+echo
+echo
+echo "NOTE: NOT INCLUDING 'user' TABLE WITH HASHED PASSWORDS IN BACKUP FILE, SAFE FOR PUBLIC GIT REPOSITORY"
+echo "NOTE: NOT INCLUDING 'user' TABLE WITH HASHED PASSWORDS IN BACKUP FILE, SAFE FOR PUBLIC GIT REPOSITORY"
+echo "NOTE: NOT INCLUDING 'user' TABLE WITH HASHED PASSWORDS IN BACKUP FILE, SAFE FOR PUBLIC GIT REPOSITORY"
+sleep 3
 /home/NEED_USERNAME/bin/mysqldump__myshinytemplate.com__no_user.sh
+sleep 3
 
 cd $DIR
 rm shinycms.conf.redacted
 cp shinycms.conf shinycms.conf.redacted
+sleep 3
 echo
 echo
-echo "DELETE PASSWORD(S) FROM CONF FILE!!!"
-echo "DELETE PASSWORD(S) FROM CONF FILE!!!"
-echo "DELETE PASSWORD(S) FROM CONF FILE!!!"
-sleep 2
+echo "WARNING: DELETE PASSWORD(S) FROM CONF FILE!!!"
+echo "WARNING: DELETE PASSWORD(S) FROM CONF FILE!!!"
+echo "WARNING: DELETE PASSWORD(S) FROM CONF FILE!!!"
+sleep 3
 vi shinycms.conf.redacted
 
 cd $DIR
 git add -A
 git commit -a
+sleep 3
+
 git push origin master
--- modified/git_backup__myshinytemplate.com__yes_user.sh
+++ modified/git_backup__myshinytemplate.com__yes_user.sh
@@ -7,21 +7,25 @@ echo
 echo "WARNING: INCLUDING 'user' TABLE WITH HASHED PASSWORDS IN BACKUP FILE!!!"
 echo "WARNING: INCLUDING 'user' TABLE WITH HASHED PASSWORDS IN BACKUP FILE!!!"
 echo "WARNING: INCLUDING 'user' TABLE WITH HASHED PASSWORDS IN BACKUP FILE!!!"
-sleep 2
+sleep 3
 /home/NEED_USERNAME/bin/mysqldump__myshinytemplate.com__yes_user.sh
+sleep 3
 
 cd $DIR
 rm shinycms.conf.redacted
 cp shinycms.conf shinycms.conf.redacted
+sleep 3
 echo
 echo
-echo "DELETE PASSWORD(S) FROM CONF FILE!!!"
-echo "DELETE PASSWORD(S) FROM CONF FILE!!!"
-echo "DELETE PASSWORD(S) FROM CONF FILE!!!"
-sleep 2
+echo "WARNING: DELETE PASSWORD(S) FROM CONF FILE!!!"
+echo "WARNING: DELETE PASSWORD(S) FROM CONF FILE!!!"
+echo "WARNING: DELETE PASSWORD(S) FROM CONF FILE!!!"
+sleep 3
 vi shinycms.conf.redacted
 
 cd $DIR
 git add -A
 git commit -a
+sleep 3
+
 git push origin master
